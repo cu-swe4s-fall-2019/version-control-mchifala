e8968196f20c145822ace1c45393c8930f4dbc71
mchifala <mich5456@colorado.edu>
2019-09-05 20:29:54 +0000

    Fixed styling errors to make code conform to PEP8 style guidelines

--- calculate.py
+++ calculate.py
@@ -1,23 +1,22 @@
 import math_lib
 import argparse
 
+
 def main(temp_F):
     tmp = math_lib.add(temp_F, -32)
     degrees_c = math_lib.div(tmp, 9/5)
     print(str(temp_F), "degrees F equals", str(degrees_c), "degrees C")
     return degrees_c
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description = "F to C temperature calculator",
-        prog = "calculate.py")
+    parser = argparse.ArgumentParser(description="F to C calculator",
+                                     prog="calculate.py")
 
     parser.add_argument("--temp_F",
-                       type = float,
-                       help = "Temperature (degrees F)",
-                       required = True)
+                        type=float,
+                        help="Temperature (degrees F)",
+                        required=True)
 
     args = parser.parse_args()
     main(args.temp_F)
-
-
